3cb997af00ad3b8b0c0bdeb6446cb2f158d3a7a5
Sean Philippi <sean.philippi@gmail.com>
2020-02-08 14:15:49 -0600

    tweak

--- monkeyscript.js
+++ monkeyscript.js
@@ -9,28 +9,28 @@
 //Adapted from Hide Reddit Side Bar script by u/pm_all_ahri_art
 // ==/UserScript==
 const hideEmailVerification = () => {
-    console.log('boom! BOOM!')
-    let first = true;
-    const verifyEmail = document.getElementsByClassName("_12Ewyh01Y1cMPB3Ri_F1C4");
-    const topBorder = document.getElementsByClassName("_25n1vjWrHyVcdDkuLwR2Y-");
-    const elements = [verifyEmail, topBorder];
-    if (verifyEmail) {
-      elements.forEach(el => el[0].setAttribute('style', 'display: none;'));
-    }
-    if (first) {
-      startLoop();
-      first = false;
-    }
+  console.log("boom! BOOM!");
+  let first = true;
+  const verifyEmail = document.querySelector("._12Ewyh01Y1cMPB3Ri_F1C4");
+  const topBorder = document.querySelector("._25n1vjWrHyVcdDkuLwR2Y-");
+  const elements = [verifyEmail, topBorder];
+  if (verifyEmail) {
+    elements.forEach(el => el.setAttribute("style", "display: none;"));
   }
-
-  const startLoop = () => {
-    console.log("I'm whirling!")
-    window.setInterval(() => {
-      hideEmailVerification();
-    }, 10000);
+  if (first) {
+    startLoop();
+    first = false;
   }
+};
 
-  window.setTimeout(() => {
-    console.log('first')
+const startLoop = () => {
+  console.log("I'm whirling!");
+  window.setInterval(() => {
     hideEmailVerification();
-  }, 3000);
+  }, 10000);
+};
+
+window.setTimeout(() => {
+  console.log("first");
+  hideEmailVerification();
+}, 2000);
